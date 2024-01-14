824c1791468797d561c921347eb4ca9f80a4db74
Mark Leighton <markconnorleighton@gmail.com>
2024-01-14 15:38:08 +0000

    Add project files.

---
+++ AoCDay10/Helper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoCDay10
+{
+    public static class Helper
+    {
+        public static IEnumerable<string> SplitBy(this string str, int chunkSize)
+        {
+            return Enumerable.Range(0, str.Length / chunkSize)
+                .Select(i => str.Substring(i * chunkSize, chunkSize));
+        }
+    }
+}
---
+++ AoCDay10/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoCDay10
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var Input = File.ReadAllText(@"C:\Users\Mark\source\repos\AoCDay10\AoCDay10\input.txt");
+            var SplitPut = Input.Split("\n");
+
+            var StartPosition = FindStart(SplitPut)
+                .Split(' ');
+            Console.WriteLine(SplitPut[int.Parse(StartPosition[0])][int.Parse(StartPosition[1])]);
+        }
+
+        static string FindStart(string[] input)
+        {
+            for (int i = 0; i < input.Length; i++) { 
+                for (int j = 0; j < input[i].Length; j++)
+                {
+                    if (input[i][j].Equals('S'))
+                    {
+                        return (i.ToString() + " " + j.ToString());
+                    }
+                }
+            }
+            return ("00");
+        }        
+    }
+}
